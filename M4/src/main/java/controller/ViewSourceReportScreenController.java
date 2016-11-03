66e5c0c6cbcf3e14b5ae40e3fbfaac87adcd8cea
Arber Muharemi <arber.muharemi@gatech.edu>
2016-11-02 23:03:48 -0400

    added fixed tableview for reports

--- M4/src/main/java/controller/ViewSourceReportScreenController.java
+++ M4/src/main/java/controller/ViewSourceReportScreenController.java
@@ -1,5 +1,6 @@
 package main.java.controller;
 
+import javafx.scene.control.TableColumn;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -7,12 +8,25 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
+import main.java.model.PurityReport;
 import main.java.model.Report;
 import main.java.model.SourceReport;
 import main.java.model.User;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
-import javax.swing.table.TableColumn;
-import java.util.ArrayList;
+import javax.xml.transform.Source;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+
 
 /**
  * Created by chitramahajani on 10/25/16.
@@ -20,18 +34,15 @@ import java.util.ArrayList;
 public class ViewSourceReportScreenController {
 
     @FXML
-    private TableView<Report> reportTable;
+    private TableView reportsTable;
 
     private Main myApp;
 
     private User currentUser;
 
-    private ArrayList<Report> mySourceReports;
+    private ObservableList<Report> mySourceReports = FXCollections.observableArrayList();
 
-    @FXML
-    private void initialize() {
-        reportTable.setEditable(false);
-    }
+    private boolean isSourceReport;
 
     public void setCurrentUser(User user) {
         currentUser = user;
@@ -40,37 +51,152 @@ public class ViewSourceReportScreenController {
     public void setMainApp(Main mainApp, boolean isSourceReport) {
         //boolean value is true for source reports false for purity reports
         myApp = mainApp;
+        System.out.println("my app is (inside setMainApp" + myApp);
+        TableColumn reportCol = new TableColumn("Report Type");
+        TableColumn reportNumCol = new TableColumn("Report Number");
+        TableColumn dateCol = new TableColumn("Date");
+        TableColumn reporterNameCol = new TableColumn("Reporter Name");
+        TableColumn locationCol = new TableColumn("Location");
+        TableColumn typeCol = new TableColumn("Water Type");
+        TableColumn conditionCol = new TableColumn("Condition");
+        TableColumn virusCol = new TableColumn("Virus");
+        TableColumn contaminationCol = new TableColumn("Contaminant");
 
-        if (isSourceReport) {
-            //viewReportsLabel.setText("Source Reports");
-            //mySourceReports = myApp.getSourceReportList();
-            ObservableList<Report> mySourceReports = FXCollections
-                    .observableArrayList(myApp.getSourceReportList());
-            //String output = "";
-            //for (Report s : mySourceReports) {
-            //    output += s + "\n\n";
-            //}
-            //viewSourceReports.setText(output);
-            //reportOverallConditionCol.setWidth(0);
-            //reportWaterConditionCol.setWidth(0);
-            //reportVirusCol.setWidth(0);
-            //reportContaminantCol.setWidth(0);
-            //reportTable.lookup("Water Condition").setVisible(false);
-            //reportTable.autosize();
-            reportTable.setItems(mySourceReports);
-        } else {
-            ObservableList<Report> mySourceReports = FXCollections
-                    .observableArrayList(myApp.getSourceReportList());
-            //viewReportsLabel.setText("Purity Reports");
-            //mySourceReports = myApp.getPurityReportList();
-            //reportWaterTypeCol.setWidth(0);
-            reportTable.setItems(mySourceReports);
-            //String output = "";
-            //for (Report s : mySourceReports) {
-            //    output += s + "\n\n";
-            //}
-            //viewSourceReports.setText(output);
-        }
+        reportCol.setCellValueFactory(
+                new Callback<TableColumn.CellDataFeatures, ObservableValue>() {
+                    @Override
+                    public ObservableValue call(TableColumn.CellDataFeatures dataFeatures) {
+                        Report report = (Report) dataFeatures.getValue();
+
+                        String type = "";
+
+                        if (report instanceof SourceReport){
+                            type = "Source";
+                        } else if (report instanceof PurityReport){
+                            type = "Purity";
+                        }
+
+                        return new SimpleStringProperty(type);
+                    }
+                }
+        );
+
+        reportNumCol.setCellValueFactory(
+                new PropertyValueFactory<>("reportNumber")
+        );
+
+        dateCol.setCellValueFactory(
+                new Callback<TableColumn.CellDataFeatures, ObservableValue>() {
+                    @Override
+                    public ObservableValue call(TableColumn.CellDataFeatures dataFeatures) {
+                        Report report = (Report) dataFeatures.getValue();
+
+                        String date = new SimpleDateFormat("mm/dd/yyyy " +
+                                "HH:mm").format(report.get_date());
+
+                        return new SimpleStringProperty(date);
+                    }
+                }
+        );
+
+        reporterNameCol.setCellValueFactory(
+                new PropertyValueFactory<>("reporterName")
+        );
+
+        locationCol.setCellValueFactory(
+                new PropertyValueFactory<>("location")
+        );
+
+        typeCol.setCellValueFactory(
+                new PropertyValueFactory<>("waterType")
+        );
+
+        conditionCol.setCellValueFactory(
+                new Callback<TableColumn.CellDataFeatures, ObservableValue>() {
+                    @Override
+                    public ObservableValue call(TableColumn.CellDataFeatures dataFeatures) {
+                        Report report = (Report) dataFeatures.getValue();
+
+                        String condition = "";
+
+                        if (report instanceof SourceReport){
+                            condition = ((SourceReport) report)
+                                    .get_waterCondition();
+                        } else if (report instanceof PurityReport){
+                            condition = ((PurityReport) report).get_waterOverallCondition();
+                        }
+
+                        return new SimpleStringProperty(condition);
+                    }
+                }
+        );
+
+        virusCol.setCellValueFactory(
+                new Callback<TableColumn.CellDataFeatures, ObservableValue>() {
+                    @Override
+                    public ObservableValue call(TableColumn.CellDataFeatures dataFeatures) {
+                        Report report = (Report) dataFeatures.getValue();
+
+                        String virus = "";
+
+                        if (report instanceof PurityReport){
+                            virus = ((PurityReport) report).get_virusPPM() + " PPM";
+                        }
+
+                        return new SimpleStringProperty(virus);
+                    }
+                }
+        );
+
+        contaminationCol.setCellValueFactory(
+                new Callback<TableColumn.CellDataFeatures, ObservableValue>() {
+                    @Override
+                    public ObservableValue call(TableColumn.CellDataFeatures dataFeatures) {
+                        Report report = (Report) dataFeatures.getValue();
+
+                        String contaminant = "";
+
+                        if (report instanceof PurityReport){
+                            contaminant = ((PurityReport) report).get_contaminantPPM() + " " +
+                                    "PPM";
+                        }
+
+                        return new SimpleStringProperty(contaminant);
+                    }
+                }
+        );
+
+        ObservableList<Report> sourceReport = FXCollections
+                .observableArrayList(myApp.getSourceReportList());
+        ObservableList<Report> purityReport = FXCollections
+                .observableArrayList(myApp.getPurityReportList());
+
+        mySourceReports = FXCollections.concat(sourceReport, purityReport);
+
+        reportsTable.setItems(mySourceReports);
+
+        reportsTable.getColumns().addAll(reportCol, reportNumCol, dateCol,
+                reporterNameCol, locationCol, typeCol, conditionCol, virusCol,
+                contaminationCol);
+    }
+
+    @FXML
+    private void handleExitClicked() {
+        myApp.loadApplication(currentUser);
+    }
+
+    @FXML
+    private void initialize() {
+
+        //if (isSourceReport) {
+        //    ObservableList<Report> mySourceReports = FXCollections
+        //            .observableArrayList(myApp.getSourceReportList());
+        //    reportTable.setItems(mySourceReports);
+        //} else {
+        //    ObservableList<Report> mySourceReports = FXCollections
+        //            .observableArrayList(myApp.getSourceReportList());
+        //    reportTable.setItems(mySourceReports);
+        //}
     }
 
     @FXML
