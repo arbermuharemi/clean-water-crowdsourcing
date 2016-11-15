e9da26559cee5618722608f006e3bf57b17b6e72
Arber Muharemi <arber.muharemi@gatech.edu>
2016-11-15 16:24:39 -0500

    adding more boilerplate for JUnit tests

--- src/test/java/ArberJUnitTests.java
+++ src/test/java/ArberJUnitTests.java
@@ -1,6 +1,7 @@
 package test.java;
 
 import main.java.controller.ApplicationController;
+import main.java.model.User;
 import org.junit.Before;
 import org.testng.annotations.Test;
 
@@ -10,18 +11,42 @@ import org.testng.annotations.Test;
  */
 public class ArberJUnitTests {
     private ApplicationController applicationController;
+    private User userType;
+    private User managerType;
+    private User adminType;
 
     @Before
-    public void setUpApplicationController() {
+    public void setupApplicationController() throws NoSuchFieldException {
         applicationController = new ApplicationController();
-        //Class<?> appControllerClass = applicationController.getClass();
-        //Field[] appControllerClassFields = appControllerClass.getFields();
-        //applicationController.submitReportButton.isDisabled();
     }
 
     @Test
-    public void checkProperThing() {
+    public void checkSubmitReportDisabledIfUserTypeUser() {
+        applicationController.setCurrentUser();
         applicationController.submitReportButton.isDisabled();
     }
 
+    @Test
+    public void checkSubmitReportDisabledIfUserTypeAdmin() {
+        applicationController.setCurrentUser();
+        applicationController.submitReportButton.isDisabled();
+    }
+
+    @Test
+    public void checkViewPurityReportAndViewHistoryReportDisabledIfUserTypeManager() {
+        applicationController.setCurrentUser();
+        applicationController.submitReportButton.isDisabled();
+    }
+
+    @Test
+    public void checkViewPurityReportAndViewHistoryReportEnabledIfUserNotTypeManager() {
+        applicationController.setCurrentUser();
+        applicationController.submitReportButton.isDisabled();
+    }
+
+    @Test
+    public void checkCreateProfileDisabledIfUserHasNoProfile() {
+        applicationController.setCurrentUser();
+        applicationController.submitReportButton.isDisabled();
+    }
 }
