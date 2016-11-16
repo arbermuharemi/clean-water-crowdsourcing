a33c7c48910a19f64c1f2f6e4e46300512ddf111
Yashveer Singh <ysingh31@gatech.edu>
2016-11-15 23:01:31 -0500

    Yash's junits, modified CreatePRofilescreenController

--- src/main/java/controller/CreateProfileScreenController.java
+++ src/main/java/controller/CreateProfileScreenController.java
@@ -46,8 +46,35 @@ public class CreateProfileScreenController {
 
     @FXML
     private void handleDonePressed() {
-        Profile profile = new Profile(emailField.getText(), phoneField.getText(), streetField.getText(), cityField.getText(), stateField.getText(), zipField.getText());
-        currentUser.addProfile(profile);
-        myApp.loadApplication(currentUser);
+        String email = emailField.getText();
+        String phone = phoneField.getText();
+        String street = streetField.getText();
+        String city = cityField.getText();
+        String state = stateField.getText();
+        String zip = zipField.getText();
+        try {
+            isValidProfile(email, phone, street, city, state, zip);
+            Profile profile = new Profile(emailField.getText(), phoneField.getText(), streetField.getText(), cityField.getText(), stateField.getText(), zipField.getText());
+            currentUser.addProfile(profile);
+            myApp.loadApplication(currentUser);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void isValidProfile(String email, String phoneNumber, String streetAddress, String city, String state, String zipCode) throws Exception {
+        String isValidEmail = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        String isValidPhone = "^\\\\(?([0-9]{3})\\\\)?[-.\\\\s]?([0-9]{3})[-.\\\\s]?([0-9]{4})$";
+        String isValidStreet = "^[0-9a-zA-Z\\.]+$";
+        String isValidCity = "^[a-zA-Z]+$";
+        String isValidState = "^[a-zA-Z]+$";
+        String isValidZip = "\\d{5}";
+        if(!email.matches(isValidEmail) || !phoneNumber.matches(isValidPhone) || !streetAddress.matches(isValidStreet)
+                || !city.matches(isValidCity) || !state.matches(isValidState) || !zipCode.matches(isValidZip)) {
+            throw new Exception("Invalid Profile Input");
+        }
+
+
     }
 }
